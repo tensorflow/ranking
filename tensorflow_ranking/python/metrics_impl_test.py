a7ddbd1d9f9b8171bda67a04afba300e2194825d
TensorFlow Ranking <no-reply@google.com>
2020-10-22 19:17:28 +0000

    Migrate NDCG metric tests from `metrics_test.py` to `metrics_impl_test.py`
    PiperOrigin-RevId: 338524038

--- tensorflow_ranking/python/metrics_impl_test.py
+++ tensorflow_ranking/python/metrics_impl_test.py
@@ -14,9 +14,14 @@
 
 """Tests for ranking metrics implementation."""
 
+import math
 import tensorflow as tf
 
 from tensorflow_ranking.python import metrics_impl
+
+
+def log2p1(x):
+  return math.log2(1. + x)
 
 
 class MRRMetricTest(tf.test.TestCase):
@@ -451,6 +456,167 @@ class MeanAveragePrecisionMetricTest(tf.test.TestCase):
       _, output_weights = metric.compute(labels, scores, weights)
 
       self.assertAllClose(output_weights, [[0.]])
+
+
+class NDCGMetricTest(tf.test.TestCase):
+
+  def test_ndcg_should_be_single_value(self):
+    with tf.Graph().as_default():
+      scores = [[3., 2., 1.]]
+      labels = [[0., 1., 0.]]
+
+      metric = metrics_impl.NDCGMetric(name=None, topn=None)
+      output, _ = metric.compute(labels, scores, None)
+
+      dcg = 1. / log2p1(2.)
+      max_dcg = 1. / log2p1(1.)
+      self.assertAllClose(output, [[dcg / max_dcg]])
+
+  def test_ndcg_should_be_0_when_no_rel_items(self):
+    with tf.Graph().as_default():
+      scores = [[3., 2., 1.]]
+      labels = [[0., 0., 0.]]
+
+      metric = metrics_impl.NDCGMetric(name=None, topn=None)
+      output, _ = metric.compute(labels, scores, None)
+
+      self.assertAllClose(output, [[0.]])
+
+  def test_ndcg_should_operate_on_graded_relevance(self):
+    with tf.Graph().as_default():
+      scores = [[4., 3., 2., 1.]]
+      labels = [[0., 3., 1., 0.]]
+
+      metric = metrics_impl.NDCGMetric(name=None, topn=None)
+      output, _ = metric.compute(labels, scores, None)
+
+      dcg = (2. ** 3. - 1.) / log2p1(2.) + 1. / log2p1(3.)
+      max_dcg = (2. ** 3. - 1.) / log2p1(1.) + 1. / log2p1(2.)
+      self.assertAllClose(output, [[dcg / max_dcg]])
+
+  def test_ndcg_should_operate_on_graded_relevance_with_custom_gain_fn(self):
+    with tf.Graph().as_default():
+      scores = [[4., 3., 2., 1.]]
+      labels = [[0., 3., 1., 0.]]
+      gain_fn = lambda label: label / 2.
+
+      metric = metrics_impl.NDCGMetric(name=None, topn=None, gain_fn=gain_fn)
+      output, _ = metric.compute(labels, scores, None)
+
+      dcg = (3. / 2.) / log2p1(2.) + (1. / 2.) / log2p1(3.)
+      max_dcg = (3. / 2.) / log2p1(1.) + (1. / 2.) / log2p1(2.)
+      self.assertAllClose(output, [[dcg / max_dcg]])
+
+  def test_ndcg_should_use_custom_rank_discount_fn(self):
+    with tf.Graph().as_default():
+      scores = [[4., 3., 2., 1.]]
+      labels = [[0., 3., 1., 0.]]
+      rank_discount_fn = lambda rank: 1.0 / (rank + 10.0)
+
+      metric = metrics_impl.NDCGMetric(name=None, topn=None,
+                                       rank_discount_fn=rank_discount_fn)
+      output, _ = metric.compute(labels, scores, None)
+
+      dcg = (2. ** 3. - 1.) / (2. + 10.) + 1. / (3. + 10.)
+      max_dcg = (2. ** 3. - 1.) / (1. + 10.) + 1. / (2. + 10.)
+      self.assertAllClose(output, [[dcg / max_dcg]])
+
+  def test_ndcg_should_ignore_padded_items(self):
+    with tf.Graph().as_default():
+      scores = [[1., 4., 3., 2.]]
+      labels = [[2., -1., 1., 0.]]
+
+      metric = metrics_impl.NDCGMetric(name=None, topn=None)
+      output, _ = metric.compute(labels, scores, None)
+
+      dcg = (2. ** 2. - 1.) / log2p1(3.) + 1. / log2p1(1.)
+      max_dcg = (2. ** 2. - 1.) / log2p1(1.) + 1. / log2p1(2.)
+      self.assertAllClose(output, [[dcg / max_dcg]])
+
+  def test_ndcg_should_be_single_value_per_list(self):
+    with tf.Graph().as_default():
+      scores = [[3., 2., 1.], [3., 1., 2.]]
+      labels = [[0., 1., 0.], [1., 1., 0.]]
+
+      metric = metrics_impl.NDCGMetric(name=None, topn=None)
+      output, _ = metric.compute(labels, scores, None)
+
+      dcg = [1. / log2p1(2.), 1. / log2p1(1.) + 1. / log2p1(3.)]
+      max_dcg = [1. / log2p1(1.), 1. / log2p1(1.) + 1. / log2p1(2.)]
+      self.assertAllClose(output,
+                          [[dcg[0] / max_dcg[0]], [dcg[1] / max_dcg[1]]])
+
+  def test_ndcg_should_handle_topn(self):
+    with tf.Graph().as_default():
+      scores = [[3., 2., 1.], [3., 2., 1.], [3., 2., 1.]]
+      labels = [[1., 0., 2.], [0., 1., 0.], [0., 0., 1.]]
+
+      metric_top1 = metrics_impl.NDCGMetric(name=None, topn=1)
+      metric_top2 = metrics_impl.NDCGMetric(name=None, topn=2)
+      metric_top6 = metrics_impl.NDCGMetric(name=None, topn=6)
+      output_top1, _ = metric_top1.compute(labels, scores, None)
+      output_top2, _ = metric_top2.compute(labels, scores, None)
+      output_top6, _ = metric_top6.compute(labels, scores, None)
+
+      max_dcg_top1 = [(2. ** 2. - 1.) / log2p1(1.),
+                      1. / log2p1(1.),
+                      1. / log2p1(1.)]
+      max_dcg = [(2. ** 2. - 1.) / log2p1(1.) + 1. / log2p1(2.),
+                 1. / log2p1(1.),
+                 1. / log2p1(1.)]
+
+      self.assertAllClose(output_top1,
+                          [[(1. / log2p1(1.)) / max_dcg_top1[0]],
+                           [0. / max_dcg_top1[1]],
+                           [0. / max_dcg_top1[2]]])
+      self.assertAllClose(output_top2,
+                          [[(1. / log2p1(1.)) / max_dcg[0]],
+                           [(1. / log2p1(2.)) / max_dcg[1]],
+                           [0. / max_dcg[2]]])
+      self.assertAllClose(output_top6,
+                          [[(1. / log2p1(1.) + (2. ** 2. - 1.) / log2p1(3.)) /
+                            max_dcg[0]],
+                           [(1. / log2p1(2.)) / max_dcg[1]],
+                           [(1. / log2p1(3.)) / max_dcg[2]]])
+
+  def test_ndcg_weights_should_be_average_of_weighted_gain(self):
+    with tf.Graph().as_default():
+      scores = [[1., 3., 2.]]
+      labels = [[1., 0., 2.]]
+      weights = [[3., 7., 9.]]
+
+      metric = metrics_impl.NDCGMetric(name=None, topn=None)
+      _, output_weights = metric.compute(labels, scores, weights)
+
+      self.assertAllClose(
+          output_weights,
+          [[(1. * 3. + (2. ** 2. - 1.) * 9.) / (1. + (2. ** 2. - 1.))]])
+
+  def test_ndcg_weights_should_be_0_when_no_rel_items(self):
+    with tf.Graph().as_default():
+      scores = [[1., 3., 2.]]
+      labels = [[0., 0., 0.]]
+      weights = [[2., 4., 4.]]
+
+      metric = metrics_impl.NDCGMetric(name=None, topn=None)
+      _, output_weights = metric.compute(labels, scores, weights)
+
+      self.assertAllClose(output_weights, [[0.]])
+
+  def test_ndcg_weights_should_use_custom_gain_fn(self):
+    with tf.Graph().as_default():
+      scores = [[1., 3., 2.]]
+      labels = [[1., 0., 2.]]
+      weights = [[3., 7., 9.]]
+      gain_fn = lambda label: label + 5.
+
+      metric = metrics_impl.NDCGMetric(name=None, topn=None, gain_fn=gain_fn)
+      _, output_weights = metric.compute(labels, scores, weights)
+
+      self.assertAllClose(
+          output_weights,
+          [[((1. + 5.) * 3. + (0. + 5.) * 7. + (2. + 5.) * 9.) /
+            ((1. + 5.) + (0. + 5.) + (2. + 5.))]])
 
 
 if __name__ == '__main__':
