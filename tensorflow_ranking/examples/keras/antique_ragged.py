2cffc29c86e243fc53362f08962550e95215663b
TensorFlow Ranking <no-reply@google.com>
2021-05-18 06:26:22 -0400

    Add example using ragged tensors with Antique dataset.
    PiperOrigin-RevId: 374380100

---
+++ tensorflow_ranking/examples/keras/antique_ragged.py
@@ -0,0 +1,245 @@
+# Copyright 2021 The TensorFlow Ranking Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+r"""Antique TFR-KPL trainer using ragged tensors.
+
+--------------------------------------------------------------------------------
+Sample command lines:
+
+MODEL_DIR=/tmp/output && \
+TRAIN=tensorflow_ranking/examples/data/train_elwc.tfrecord && \
+EVAL=tensorflow_ranking/examples/data/eval_elwc.tfrecord && \
+VOCAB=tensorflow_ranking/examples/data/vocab.txt && \
+rm -rf $MODEL_DIR && \
+bazel build -c opt \
+tensorflow_ranking/examples/keras/antique_ragged && \
+./bazel-bin/tensorflow_ranking/examples/keras/antique_ragged \
+--train_file_pattern=$TRAIN \
+--eval_file_pattern=$EVAL \
+--vocab_file_path=$VOCAB \
+--model_dir=$MODEL_DIR
+
+You can use TensorBoard to display the training results stored in $MODEL_DIR.
+
+Notes:
+  * Use --alsologtostderr if the output is not printed into screen.
+"""
+import os
+from typing import List
+
+from absl import app
+from absl import flags
+from absl import logging
+import tensorflow as tf
+
+import tensorflow_ranking as tfr
+
+flags.DEFINE_string("train_input_pattern", None,
+                    "Input file path pattern used for training.")
+
+flags.DEFINE_string("eval_input_pattern", None,
+                    "Input file path pattern used for eval.")
+
+flags.DEFINE_string("test_input_pattern", None,
+                    "Input file path pattern used for test.")
+
+flags.DEFINE_string(
+    "vocab_file_path", None, "Path to vocab file used for tokenizing the "
+    "Antique dataset.")
+
+flags.DEFINE_integer(
+    "vocab_size", 30522, "Size of the vocab file used for "
+    "tokenizing the Antique dataset.")
+
+flags.DEFINE_float("learning_rate", 0.005, "Learning rate for optimizer.")
+
+flags.DEFINE_multi_integer("hidden_layer_dims", [20, 10],
+                           "Number of units in each hidden layer.")
+
+flags.DEFINE_integer("train_batch_size", 16,
+                     "Number of input records used per batch for training.")
+
+flags.DEFINE_integer("eval_batch_size", 64,
+                     "Number of input records used per batch for eval.")
+
+flags.DEFINE_integer("num_epochs", 100,
+                     "Number of passes over the training data.")
+
+flags.DEFINE_string("loss", tfr.keras.losses.RankingLossKey.APPROX_NDCG_LOSS,
+                    "See tfr.keras.losses.RankingLossKey.")
+
+flags.DEFINE_integer("embedding_dimension", 20, "Size of embedding.")
+
+flags.DEFINE_string(
+    "model_dir", None, "The directory where the model weights and "
+    "training/evaluation summaries are stored.")
+
+FLAGS = flags.FLAGS
+
+# The document relevance label and mask feature.
+_LABEL_FEATURE = "relevance"
+_MASK = "example_list_mask"
+
+# Context and example features.
+_CONTEXT_RAGGED_FEATURE_KEYS = ["query_tokens"]
+_EXAMPLE_RAGGED_FEATURE_KEYS = ["document_tokens"]
+
+
+class AntiqueEmbeddingRankingModel(tf.keras.Model):
+  """A text-embedding ranking model for the Antique dataset.
+
+  This class implements a ranking model that uses word embeddings followed by a
+  feedforward neural network to produce document scores.
+  """
+
+  def __init__(self, vocab_size: int, vocab_file_path: str, embedding_dim: int,
+               hidden_dims: List[int]):
+    super().__init__()
+    self._lookup = tf.keras.layers.experimental.preprocessing.StringLookup(
+        max_tokens=vocab_size,
+        vocabulary=vocab_file_path,
+        num_oov_indices=10,
+        mask_token=None)
+
+    self._embedding = tf.keras.layers.Embedding(
+        input_dim=vocab_size + 10,
+        output_dim=embedding_dim,
+        embeddings_initializer=None,
+        embeddings_constraint=None)
+
+    tower = tf.keras.Sequential([tf.keras.layers.BatchNormalization()])
+    for hidden_dim in hidden_dims:
+      tower.add(tf.keras.layers.Dense(hidden_dim, activation=tf.nn.relu))
+    tower.add(tf.keras.layers.Dense(1))
+
+    # A `TimeDistributed` layer will apply the tower model to each entry in the
+    # second dimension of the input tensor independently and handles ragged
+    # tensors. This makes it possible to compute a score for each document.
+    self._ragged_tower = tf.keras.layers.TimeDistributed(tower)
+
+  def call(self, inputs):
+    # Get query and document tokens.
+    query_tokens, doc_tokens = inputs["query_tokens"], inputs["document_tokens"]
+
+    # Embed query and document tokens via a shared embedding layer.
+    query_token_ids = self._lookup(query_tokens)
+    doc_token_ids = self._lookup(doc_tokens)
+    query_embeddings = self._embedding(query_token_ids)
+    doc_embeddings = self._embedding(doc_token_ids)
+
+    # Compute average embedding by averaging the token embeddings.
+    query_embeddings = tf.reduce_mean(query_embeddings, axis=1)
+    doc_embeddings = tf.reduce_mean(doc_embeddings, axis=2)
+
+    # Broadcast query embedding over all documents and concatenate embeddings.
+    query_embeddings = tf.ones_like(doc_embeddings) * tf.expand_dims(
+        query_embeddings, axis=1)
+    final_embeddings = tf.concat([query_embeddings, doc_embeddings], axis=2)
+
+    # Run concatenated embeddings through a feed-forward scoring tower and
+    # return the result.
+    scores = self._ragged_tower(final_embeddings)
+    return tf.squeeze(scores, axis=2)
+
+
+def _add_ragged_label(inputs):
+  mask = inputs[_MASK]
+  features_dict = {
+      "query_tokens": inputs["query_tokens"],
+      "document_tokens": inputs["document_tokens"]
+  }
+  label = tf.ragged.boolean_mask(inputs[_LABEL_FEATURE], mask)
+  label = tf.cast(label, dtype=tf.float32)
+  return features_dict, label
+
+
+def train_and_eval():
+  """Train and evaluate ranking model."""
+  # Create optimizer, ranking loss and ranking metrics.
+  optimizer = tf.keras.optimizers.Adagrad(learning_rate=FLAGS.learning_rate)
+  loss = tfr.keras.losses.get(loss=FLAGS.loss, ragged=True)
+  eval_metrics = tfr.keras.metrics.default_keras_metrics(ragged=True)
+
+  # Create ranking model to train. This model operates on ragged tensors and
+  # returns model scores as ragged tensors.
+  model = AntiqueEmbeddingRankingModel(
+      vocab_size=FLAGS.vocab_size,
+      vocab_file_path=FLAGS.vocab_file_path,
+      embedding_dim=FLAGS.embedding_dimension,
+      hidden_dims=FLAGS.hidden_layer_dims)
+  model.compile(optimizer=optimizer, loss=loss, metrics=eval_metrics)
+
+  # Create feature specification.
+  context_feature_spec = {
+      "query_tokens": tf.io.RaggedFeature(dtype=tf.string),
+  }
+  example_feature_spec = {
+      "document_tokens": tf.io.RaggedFeature(dtype=tf.string),
+      _LABEL_FEATURE: tf.io.FixedLenFeature(shape=[], dtype=tf.int64,
+                                            default_value=0)
+  }
+
+  # Load datasets.
+  train_dataset = tfr.data.build_ranking_dataset(
+      file_pattern=FLAGS.train_input_pattern,
+      data_format=tfr.data.ELWC,
+      batch_size=FLAGS.train_batch_size,
+      context_feature_spec=context_feature_spec,
+      example_feature_spec=example_feature_spec,
+      mask_feature_name=_MASK,
+      num_epochs=1,
+      shuffle_buffer_size=1000)
+  eval_dataset = tfr.data.build_ranking_dataset(
+      file_pattern=FLAGS.eval_input_pattern,
+      data_format=tfr.data.ELWC,
+      batch_size=FLAGS.eval_batch_size,
+      context_feature_spec=context_feature_spec,
+      example_feature_spec=example_feature_spec,
+      mask_feature_name=_MASK,
+      num_epochs=1,
+      shuffle_buffer_size=1000)
+
+  # This maps the dataset features to a tuple (features, label) where the label
+  # is converted to a ragged tensor.
+  train_dataset = train_dataset.map(_add_ragged_label)
+  eval_dataset = eval_dataset.map(_add_ragged_label)
+
+  # Train ranker.
+  logging.info("Training the model...")
+  tensorboard_callback = tf.keras.callbacks.TensorBoard(FLAGS.model_dir)
+  model.fit(
+      train_dataset,
+      epochs=FLAGS.num_epochs,
+      validation_data=eval_dataset,
+      callbacks=[tensorboard_callback])
+  logging.info("Finished training the model.")
+
+  # Save model.
+  saved_model_path = os.path.join(FLAGS.model_dir, "export/")
+  logging.info("Exporting to SavedModel...")
+  model.save(filepath=saved_model_path)
+  logging.info("SavedModel exported successfully to: %s", saved_model_path)
+
+
+def main(_):
+  tf.random.set_seed(1234)
+  train_and_eval()
+
+
+if __name__ == "__main__":
+  flags.mark_flag_as_required("train_input_pattern")
+  flags.mark_flag_as_required("eval_input_pattern")
+  flags.mark_flag_as_required("vocab_file_path")
+  app.run(main)
---
+++ tensorflow_ranking/examples/keras/antique_ragged_test.py
@@ -0,0 +1,122 @@
+# Copyright 2021 The TensorFlow Ranking Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for antique_kpl_din.py."""
+
+import os
+
+from absl.testing import flagsaver
+import tensorflow as tf
+
+from google.protobuf import text_format
+from tensorflow_ranking.examples.keras import antique_ragged
+from tensorflow_serving.apis import input_pb2
+
+ELWC = text_format.Parse(
+    """
+    context {
+      features {
+        feature {
+          key: "query_tokens"
+          value { bytes_list { value: ["this", "is", "a", "relevant", "question"] } }
+        }
+      }
+    }
+    examples {
+      features {
+        feature {
+          key: "document_tokens"
+          value { bytes_list { value: ["this", "is", "a", "relevant", "answer"] } }
+        }
+        feature {
+          key: "relevance"
+          value { int64_list { value: 1 } }
+        }
+      }
+    }
+    examples {
+      features {
+        feature {
+          key: "document_tokens"
+          value { bytes_list { value: ["irrelevant", "data"] } }
+        }
+        feature {
+          key: "relevance"
+          value { int64_list { value: 0 } }
+        }
+      }
+    }""", input_pb2.ExampleListWithContext())
+
+VOCAB = [
+    "this", "is", "a", "relevant", "question", "answer", "irrelevant", "data"
+]
+
+
+class ModelTrainAndEvaluateTest(tf.test.TestCase):
+
+  def test_train_and_eval(self):
+    # `create_tempdir` creates an isolated directory for this test, and
+    # will be properly cleaned up by the test.
+    data_dir = self.create_tempdir()
+    data_file = os.path.join(data_dir, "elwc.tfrecord")
+    vocab_file = os.path.join(data_dir, "vocab.txt")
+    model_dir = os.path.join(data_dir, "model")
+
+    # Save data.
+    with tf.io.TFRecordWriter(data_file) as writer:
+      for _ in range(10):
+        writer.write(ELWC.SerializeToString())
+
+    with tf.io.gfile.GFile(vocab_file, "w") as writer:
+      writer.write("\n".join(VOCAB) + "\n")
+
+    with flagsaver.flagsaver(
+        train_input_pattern=data_file,
+        eval_input_pattern=data_file,
+        model_dir=model_dir,
+        vocab_file_path=vocab_file,
+        train_batch_size=2,
+        eval_batch_size=2,
+        num_epochs=5,
+        vocab_size=len(VOCAB),
+        hidden_layer_dims=[16, 8]):
+
+      # Train and eval model.
+      tf.random.set_seed(1234)
+      antique_ragged.train_and_eval()
+
+      # Check SavedModel is exported correctly.
+      saved_model_path = os.path.join(model_dir, "export/")
+      self.assertTrue(tf.saved_model.contains_saved_model(saved_model_path))
+
+    # Check SavedModel can be loaded and called with ragged tensors.
+    saved_model = tf.keras.models.load_model(saved_model_path)
+    inputs = {
+        "query_tokens": tf.ragged.constant(
+            [["this", "is", "a", "relevant", "question"]],
+            row_splits_dtype=tf.int32),
+        "document_tokens": tf.ragged.constant(
+            [[["this", "is", "a", "relevant", "answer"],
+              ["irrelevant", "data"]]],
+            row_splits_dtype=tf.int32),
+    }
+    scores = saved_model(inputs)
+
+    # Assert model output is a ragged tensor and has the correct contents.
+    self.assertIsInstance(scores, tf.RaggedTensor)
+    self.assertGreater(scores[0, 0], scores[0, 1])
+
+
+if __name__ == "__main__":
+  tf.test.main()
