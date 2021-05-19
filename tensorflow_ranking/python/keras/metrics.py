e4be5af7ca18990c1bb15101f4da3f8f78305e06
TensorFlow Ranking <no-reply@google.com>
2021-05-19 05:54:35 -0400

    Internal change
    PiperOrigin-RevId: 374605789

--- tensorflow_ranking/python/keras/metrics.py
+++ tensorflow_ranking/python/keras/metrics.py
@@ -466,7 +466,68 @@ class PrecisionIAMetric(_RankingMetric):
 
 @tf.keras.utils.register_keras_serializable(package="tensorflow_ranking")
 class MeanAveragePrecisionMetric(_RankingMetric):
-  """Implements mean average precision (MAP)."""
+  r"""Mean average precision (MAP).
+
+  For each list of scores `s` in `y_pred` and list of labels `y` in `y_true`:
+
+  ```
+  MAP(y, s) = sum_k (P@k(y, s) * rel(k)) / sum_i y_i
+  rel(k) = y_i if rank(s_i) = k
+  ```
+
+  NOTE: This metric converts graded relevance to binary relevance by setting
+  `y_i = 1` if `y_i >= 1`.
+
+  Standalone usage:
+
+  >>> y_true = [[0., 1., 1.]]
+  >>> y_pred = [[3., 1., 2.]]
+  >>> map_metric = tfr.keras.metrics.MeanAveragePrecisionMetric(topn=2)
+  >>> map_metric(y_true, y_pred).numpy()
+  0.25
+
+  >>> # Using ragged tensors
+  >>> y_true = tf.ragged.constant([[0., 1.], [1., 2., 0.]])
+  >>> y_pred = tf.ragged.constant([[2., 1.], [2., 5., 4.]])
+  >>> map_metric = tfr.keras.metrics.MeanAveragePrecisionMetric(
+  ...   topn=2, ragged=True)
+  >>> map_metric(y_true, y_pred).numpy()
+  0.5
+
+  Usage with the `compile()` API:
+
+  ```python
+  model.compile(optimizer='sgd',
+                metrics=[tfr.keras.metrics.MeanAveragePrecisionMetric()])
+  ```
+
+  Definition:
+
+  $$
+  \text{MAP}(\{y\}, \{s\}) =
+  \frac{\sum_k P@k(y, s) \cdot \text{rel}(k)}{\sum_j \bar{y}_j} \\
+  \text{rel}(k) = \max_i I[\text{rank}(s_i) = k] \bar{y}_i
+  $$
+
+  where:
+
+  * $$P@k(y, s)$$ is the Precision at rank $$k$$. See
+    `tfr.keras.metrics.PrecisionMetric`.
+  * $$\text{rank}(s_i)$$ is the rank of item $$i$$ after sorting by scores $$s$$
+    with ties broken randomly
+  * $$I[]$$ is the indicator function:\
+    $$I[\text{cond}] = \begin{cases}
+    1 & \text{if cond is true}\\
+    0 & \text{else}\end{cases}
+    $$
+  * $$\bar{y}_i$$ are the truncated labels:\
+    $$
+    \bar{y}_i = \begin{cases}
+    1 & \text{if }y_i \geq 1 \\
+    0 & \text{else}
+    \end{cases}
+    $$
+  """
 
   def __init__(self, name=None, topn=None, dtype=None, ragged=False, **kwargs):
     super(MeanAveragePrecisionMetric, self).__init__(
