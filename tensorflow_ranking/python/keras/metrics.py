8e91e1b9d2619ae246c6297bbd22fef8f851400c
TensorFlow Ranking <no-reply@google.com>
2021-05-19 05:53:38 -0400

    Internal change
    PiperOrigin-RevId: 374605631

--- tensorflow_ranking/python/keras/metrics.py
+++ tensorflow_ranking/python/keras/metrics.py
@@ -512,7 +512,53 @@ class AlphaDCGMetric(_RankingMetric):
 
 @tf.keras.utils.register_keras_serializable(package="tensorflow_ranking")
 class OPAMetric(_RankingMetric):
-  """Implements ordered pair accuracy (OPA)."""
+  r"""Ordered pair accuracy (OPA).
+
+  For each list of scores `s` in `y_pred` and list of labels `y` in `y_true`:
+
+  ```
+  OPA(y, s) = sum_i sum_j I[s_i > s_j] I[y_i > y_j] / sum_i sum_j I[y_i > y_j]
+  ```
+
+  NOTE: Pairs with equal labels (`y_i = y_j`) are always ignored. Pairs with
+  equal scores (`s_i = s_j`) are considered incorrectly ordered.
+
+  Standalone usage:
+
+  >>> y_true = [[0., 1., 2.]]
+  >>> y_pred = [[3., 1., 2.]]
+  >>> opa = tfr.keras.metrics.OPAMetric()
+  >>> opa(y_true, y_pred).numpy()
+  0.33333334
+
+  >>> # Using ragged tensors
+  >>> y_true = tf.ragged.constant([[0., 1.], [1., 2., 0.]])
+  >>> y_pred = tf.ragged.constant([[2., 1.], [2., 5., 4.]])
+  >>> opa = tfr.keras.metrics.OPAMetric(ragged=True)
+  >>> opa(y_true, y_pred).numpy()
+  0.5
+
+  Usage with the `compile()` API:
+
+  ```python
+  model.compile(optimizer='sgd', metrics=[tfr.keras.metrics.OPAMetric()])
+  ```
+
+  Definition:
+
+  $$
+  \text{OPA}(\{y\}, \{s\}) =
+  \frac{\sum_i \sum_j I[s_i > s_j] I[y_i > y_j]}{\sum_i \sum_j I[y_i > y_j]}
+  $$
+
+  where $$I[]$$ is the indicator function:
+
+  $$
+  I[\text{cond}] = \begin{cases}
+  1 & \text{if cond is true}\\
+  0 & \text{else}\end{cases}
+  $$
+  """
 
   def __init__(self, name=None, dtype=None, ragged=False, **kwargs):
     super(OPAMetric, self).__init__(name=name, dtype=dtype, **kwargs)
