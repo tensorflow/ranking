7564308368397ff2b5ab1eebac1cf69a6db7a71c
TensorFlow Ranking <no-reply@google.com>
2021-05-19 05:55:13 -0400

    Internal change
    PiperOrigin-RevId: 374605873

--- tensorflow_ranking/python/keras/metrics.py
+++ tensorflow_ranking/python/keras/metrics.py
@@ -586,11 +586,57 @@ class NDCGMetric(_RankingMetric):
 
 @tf.keras.utils.register_keras_serializable(package="tensorflow_ranking")
 class DCGMetric(_RankingMetric):
-  """Implements discounted cumulative gain (DCG).
+  r"""Discounted cumulative gain (DCG).
 
-  The `gain_fn` and `rank_discount_fn` should be keras serializable. Please see
-  the `pow_minus_1` and `log2_inverse` above as examples when defining user
-  customized functions.
+  Discounted cumulative gain ([Järvelin et al, 2002][jarvelin2002]).
+
+  For each list of scores `s` in `y_pred` and list of labels `y` in `y_true`:
+
+  ```
+  DCG(y, s) = sum_i gain(y_i) * rank_discount(rank(s_i))
+  ```
+
+  NOTE: The `gain_fn` and `rank_discount_fn` should be keras serializable.
+  Please see `tfr.keras.utils.pow_minus_1` and `tfr.keras.utils.log2_inverse` as
+  examples when defining user customized functions.
+
+  Standalone usage:
+
+  >>> y_true = [[0., 1., 1.]]
+  >>> y_pred = [[3., 1., 2.]]
+  >>> dcg = tfr.keras.metrics.DCGMetric()
+  >>> dcg(y_true, y_pred).numpy()
+  1.1309297
+
+  >>> # Using ragged tensors
+  >>> y_true = tf.ragged.constant([[0., 1.], [1., 2., 0.]])
+  >>> y_pred = tf.ragged.constant([[2., 1.], [2., 5., 4.]])
+  >>> dcg = tfr.keras.metrics.DCGMetric(ragged=True)
+  >>> dcg(y_true, y_pred).numpy()
+  2.065465
+
+  Usage with the `compile()` API:
+
+  ```python
+  model.compile(optimizer='sgd', metrics=[tfr.keras.metrics.DCGMetric()])
+  ```
+
+  Definition:
+
+  $$
+  \text{DCG}(\{y\}, \{s\}) =
+  \sum_i \text{gain}(y_i) \cdot \text{rank_discount}(\text{rank}(s_i))
+  $$
+
+  where $$\text{rank}(s_i)$$ is the rank of item $$i$$ after sorting by scores
+  $$s$$ with ties broken randomly.
+
+  References:
+
+    - [Cumulated gain-based evaluation of IR techniques, Järvelin et al,
+       2002][jarvelin2002]
+
+  [jarvelin2002]: https://dl.acm.org/doi/10.1145/582415.582418
   """
 
   def __init__(self,
