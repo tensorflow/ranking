f4af081181839182dc0887b2f4dfa1aadc3b2457
Rama Pasumarthi <ramakumar@google.com>
2021-10-20 14:23:35 -0400

    Add flags for `code_url_prefix`, `site_path` and `search_hints`.
    PiperOrigin-RevId: 404598545

--- docs/build_docs.py
+++ docs/build_docs.py
@@ -45,27 +45,38 @@ del tfr.metrics.metrics_impl
 FLAGS = flags.FLAGS
 
 
+flags.DEFINE_string('output_dir', '/tmp/tfr_api',
+                    'Where to write the resulting docs to.')
+flags.DEFINE_string(
+    'code_url_prefix',
+    ('https://github.com/tensorflow/ranking/tree/master/tensorflow_ranking'),
+    'The url prefix for links to code.')
+
+flags.DEFINE_bool('search_hints', True,
+                  'Include metadata search hints in the generated files.')
+
+flags.DEFINE_string('site_path', 'ranking/api_docs/python',
+                    'Path prefix in the _toc.yaml')
+
+
 def build_docs(output_dir):
-  """Build api docs for tensorflow_datasets."""
+  """Build api docs for TensorFlow Ranking package."""
   doc_generator = generate_lib.DocGenerator(
-      root_title="TensorFlow Ranking",
-      py_modules=[("tfr", tfr)],
+      root_title='TensorFlow Ranking',
+      py_modules=[('tfr', tfr)],
       base_dir=os.path.dirname(tfr.__file__),
-      search_hints=True,
-      code_url_prefix="https://github.com/tensorflow/ranking/tree/master/tensorflow_ranking",
-      site_path="ranking/api_docs/python",
+      search_hints=FLAGS.search_hints,
+      code_url_prefix=FLAGS.code_url_prefix,
+      site_path=FLAGS.site_path,
       callbacks=[public_api.local_definitions_filter])
 
   doc_generator.build(output_dir)
+  print('Output docs to: ', FLAGS.output_dir)
 
 
-def _main(_):
+def main(_):
   build_docs(FLAGS.output_dir)
 
 
-if __name__ == "__main__":
-  flags.DEFINE_string("output_dir",
-                      "/tmp/tfr_api",
-                      "Output directory.")
-
-  app.run(_main)
+if __name__ == '__main__':
+  app.run(main)
